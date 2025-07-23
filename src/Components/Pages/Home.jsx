9aa5709073aa98703e3ac620a1e608e8d95a6482
zayed khan <zayedk458@gmail.com>
2025-07-24 01:00:11 +0600

    first commit

---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Main from './Main';
+
+const Home = () => {
+    return (
+        <>
+        <div className='flex'>
+            
+            <Sidebar></Sidebar>
+            <Main></Main>
+
+        </div>
+            
+        </>
+    );
+};
+
+export default Home;
---
+++ src/Components/Pages/Sidebar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { assets } from '../../assets/assets';
+
+
+const Sidebar = () => {
+    const [extended,setExtended]=useState(false);
+    return (
+        <div className=' '>
+
+            <div className=' min-h-screen inline-flex flex-col justify-between bg-gray-100 py-5 px-5 '>
+
+                <div>
+                    <div>
+                        <img onClick={()=>{setExtended(!extended)}} className='w-[30px] ' src={assets.menu_icon} alt="" />
+                        <div className='inline-flex items-center text-gray-600 gap-2.5 py-2 px-3 mt-13 mb-3 bg-gray-200 rounded-3xl cursor-pointer'>
+                            <img className='w-[20px]' src={assets.plus_icon} alt="" />
+                           {extended ?<p >New Chat</p>:null}  
+                        </div>
+                    </div>
+                    {extended? <div className='flex flex-col'>
+                        <p className='text-start mt-4'>Recent</p>
+                        <div className='cursor-pointer inline-flex items-center text-gray-600 gap-2 mt-4 border-none rounded-2xl hover:bg-gray-400 p-2'>
+                            <img className='w-[25px] ' src={assets.message_icon} alt="" />
+                            <p>What is react ...</p>
+                        </div>
+                    </div>:null}
+                   
+                </div>
+
+
+                <div className='flex flex-col '>
+                    <div className='inline-flex items-center gap-2 border-none rounded-2xl hover:bg-gray-400 p-2 cursor-pointer'>
+                        <img className='w-[25px]' src={assets.question_icon} alt="" />
+                       {extended?<p>Help </p>:null } 
+                    </div>
+                    <div className='inline-flex items-center gap-2 border-none rounded-2xl hover:bg-gray-400 p-2 cursor-pointer'>
+                        <img className='w-[25px]' src={assets.history_icon} alt="" />
+                        {extended?<p>Activity </p>:null } 
+                    </div>
+                    <div className='inline-flex items-center gap-2 border-none rounded-2xl hover:bg-gray-400 p-2 cursor-pointer'>
+                        <img className='w-[25px]' src={assets.setting_icon} alt="" />
+                       {extended?<p>Seetings </p>:null } 
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Sidebar;
